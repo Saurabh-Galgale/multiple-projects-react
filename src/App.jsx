24a2ea12c49d95407140528932b4f9768053391f
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2025-01-28 20:46:15 +0530

    UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
 import "./app.css";
+import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import OtpGenerator from "./components/OtpGenerator";
+import Hello from "./components/Hello";
+
+const components = [OtpGenerator, Hello];
 
 export default function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextProject = () => {
+    setCurrentIndex((prev) => (prev + 1) % components.length);
+  };
+
+  const prevProject = () => {
+    setCurrentIndex(
+      (prev) => (prev - 1 + components.length) % components.length
+    );
+  };
+
+  const CurrentComponent = components[currentIndex];
+
   return (
-    <div className="appDiv">
-      <h1>Hello! APP...</h1>
+    <div className="container">
+      <ArrowCircleLeftOutlinedIcon
+        fontSize="large"
+        className="navButton left"
+        onClick={prevProject}
+      />
+
+      <div className="appDiv">
+        <CurrentComponent />
+      </div>
+
+      <ArrowCircleRightOutlinedIcon
+        fontSize="large"
+        className="navButton left"
+        onClick={nextProject}
+      />
     </div>
   );
 }
---
+++ src/components/OtpGenerator.jsx
@@ -0,0 +1,3 @@
+export default function OtpGenerator() {
+  return <div>OTP Generator Component</div>;
+}
